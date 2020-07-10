22a12ac0da64c5edf90a95d59068273da917b021
Benjamin Koehler <bkoehler751@gmail.com>
2020-07-10 07:11:46 -0700

    setup test

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
-
-
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const stickers = require('./api/stickers')
+const stickers = require('./api/stickers');
 
-
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/v1/stickers', stickers);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
   res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {}
-  })
+  });
 });
 
 module.exports = app;
